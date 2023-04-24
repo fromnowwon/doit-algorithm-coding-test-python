d95a6490d7b1c87ac66c7442676281239487df91
cw <fromnowwon@gmail.com>
2023-04-24 21:18:32 +0900

    refactor: 002_1546_평균구하기

--- 002_1546_평균구하기.py
+++ 002_1546_평균구하기.py
@@ -1,11 +1,6 @@
-# n에 과목 수를 입력한다
-# mylist에 점수 정보를 저장한다
-# mymax에 최댓값을 저장한다
-# sum에 합을 저장한다
-# sum / mymax * 100 / n
-
 n = input()
 mylist = list(map(int, input().split()))
 mymax = max(mylist)
 sum = sum(mylist)
-print((sum / mymax * 100) / int(n))
+
+print(sum / mymax * 100 / int(n))
